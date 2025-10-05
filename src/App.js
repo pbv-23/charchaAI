5bfcaa863894d11b6eb509526ab975148d819b21
Sreeja <dusasreeja04@gmail.com>
2025-10-05 12:37:05 +0530

    App page changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import React from "react";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
